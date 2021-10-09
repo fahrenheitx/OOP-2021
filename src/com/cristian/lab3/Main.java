3307a3ed2d785f8bf121f38b83f15dd48b38441f
Cristian Ionel <cristian.ionel1@isa.utm.md>
2021-10-09 15:53:35 +0300

    lab3 done

--- src/com/cristian/lab3/Main.java
+++ src/com/cristian/lab3/Main.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         TextManipulation someString = new TextManipulation();
-        someString.text = "test test. test??? yes no yes test no, of course, noo no test.";
+        someString.text = "TEst test. tesdNdt??? yes no yeS Test no, of course, noo no test.";
         System.out.println("Number of words : " + someString.getWords());
         System.out.println("Number of sentences : " + someString.getSentences());
         System.out.println("Number of letters : " + someString.getLetters());
         System.out.println("Number of vowels : " + someString.getVowels());
         System.out.println("Number of consonants : " + someString.getConsonants());
         System.out.println("Longest word : " + someString.getLongestWord());
+        System.out.println("Top words : " + someString.topWords());
     }
 }
--- src/com/cristian/lab3/TextManipulation.java
+++ src/com/cristian/lab3/TextManipulation.java
@@ -37,8 +37,11 @@ public class TextManipulation {
     public int getVowels() {
         int count = 0;
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || text.charAt(i) == 'i'
-                    || text.charAt(i) == 'u' || text.charAt(i) == 'o')
+            if (text.toLowerCase().charAt(i) == 'a'
+                    || text.toLowerCase().charAt(i) == 'e'
+                    || text.toLowerCase().charAt(i) == 'i'
+                    || text.toLowerCase().charAt(i) == 'u'
+                    || text.toLowerCase().charAt(i) == 'o')
                 count++;
         }
         return count;
@@ -50,6 +53,9 @@ public class TextManipulation {
 
     public String getLongestWord() {
         String[] words = this.text.split("[\s.,;?!]+"); // splitting string in array of substrings (words)
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+        }
         int maxLength = -1;
         String longestWord = "";
         for (String a : words) {
@@ -59,5 +65,32 @@ public class TextManipulation {
             }
         }
         return longestWord;
+    }
+
+    public List<String> topWords() {
+        String[] words = this.text.split("[\s.,;?!]+");
+        Map<String, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+        }
+
+        Map<String, Integer> count = new HashMap<>();
+        for (String word : words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<String> maxHeap = new PriorityQueue<>(
+                (str1, str2) -> {
+                    if (count.get(str1).equals(count.get(str2))) {
+                        return str1.compareTo(str2);
+                    } else {
+                        return -Integer.compare(count.get(str1), count.get(str2));
+                    }
+                });
+        maxHeap.addAll(count.keySet());
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            result.add(maxHeap.remove());
+        }
+        return result;
     }
 }
